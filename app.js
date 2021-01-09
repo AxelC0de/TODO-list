d27509d8a8898e78ebb7d46da9124a65b7421def
Redknight777999 <65612144+Redknight777999@users.noreply.github.com>
2021-01-09 20:02:29 +0300

    delete: state

--- app.js
+++ app.js
@@ -106,14 +106,3 @@ CLEAR_BTN.addEventListener('click', () => {
 	state.todos = [];
 	renderTodos();
 })
-
-
-let testFunc=async ()=>{
-	try {
-	let response = await fetch('http://localhost:3000/?q=getstate&name=aleksey')
-	}
-	catch (e){
-		console.log(e)
-	}
-};
-document.getElementById('testBtn').addEventListener('click',testFunc)
